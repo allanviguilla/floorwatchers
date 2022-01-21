354bee15b3f85f2f468c8afc6de9034e23de72c7
Khalil Brown <knb6@njit.edu>
2022-01-20 20:54:40 -0500

    roadmap, mint

--- src/components/Mint/index.tsx
+++ src/components/Mint/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
-import { Button, Grid} from "@mui/material";
+import { Button, Grid } from "@mui/material";
 
-export default function Mint(){
-	return(
+export default function Mint() {
+	return (
 		<Grid
 			container
 			justifyContent={'center'}
@@ -11,10 +11,18 @@ export default function Mint(){
 			spacing={'column'}
 			marginTop={'30px'}
 		>
-			<Typography variant="h2">Mint</Typography>
-			<Grid container justifyContent={'center'}>
+			<Grid item xs={12}>
+				<Typography variant="h2" align="center">Mint</Typography>
+			</Grid>
+
+			<Grid item xs={6} sx={{marginTop:'20px'}}>
+				<Typography variant="h6" align="center">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				</Typography>
+			</Grid>
+			<Grid container justifyContent={'center'} sx={{marginTop:'20px'}}>
 				<Grid item>
-					<Button variant="contained">Mint</Button>
+					<Button variant="contained" size="large">Mint</Button>
 				</Grid>
 			</Grid>
 		</Grid>
