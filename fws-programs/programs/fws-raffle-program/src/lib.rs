7d7c5a9eadbd817c7db67e51f238e95f41d91117
Khalil Brown <knb6@njit.edu>
2022-01-25 01:22:28 -0500

    cleanup

--- fws-programs/programs/fws-raffle-program/src/lib.rs
+++ fws-programs/programs/fws-raffle-program/src/lib.rs
@@ -38,15 +38,15 @@ pub mod fws_raffle_program {
 
 }
 
-// #[derive(Accounts)]
-// pub struct Create<'info> {
-// 	#[account(signer)]
-// 	authority: AccountInfo<'info>,
-// 	#[account(zero)]
-// 	raffle_account: Account<'info, RaffleAccount>,
-// 	reward_mint: Account<'info, Mint>,
-// 	reward_authority: AccountInfo<'info>
-// }
+#[derive(Accounts)]
+pub struct Create<'info> {
+	#[account(signer)]
+	authority: AccountInfo<'info>,
+	#[account(zero)]
+	raffle_account: Account<'info, RaffleAccount>,
+	reward_mint: Account<'info, Mint>,
+	reward_authority: AccountInfo<'info>
+}
 
 #[derive(Accounts)]
 #[instruction(mints: Vec<Vec<u8>>)]
@@ -55,46 +55,23 @@ pub struct Draw<'info> {
 	authority: AccountInfo<'info>,
 }
 
-// #[derive(Accounts)]
-// pub struct Claim<'info> {
-// 	#[account(signer,
-// 	constraint = raffle_account.to_account_info().key == authority.key )]
-// 	authority: AccountInfo<'info>, //winner of the raffle
-// 	raffle_account: Account<'info, RaffleAccount>,
-// }
+#[derive(Accounts)]
+pub struct Claim<'info> {
+	#[account(signer,
+	constraint = raffle_account.to_account_info().key == authority.key )]
+	authority: AccountInfo<'info>, //winner of the raffle
+	raffle_account: Account<'info, RaffleAccount>,
+}
 
-// #[derive(Accounts)]
-// pub struct SetMintAuth<'info> {
-// 	#[account(signer)]
-// 	authority: AccountInfo<'info>,
-// 	mint_update_authority: AccountInfo<'info>,
-// 	raffle_account: Account<'info, RaffleAccount>,
-// }
-// #[derive(Accounts)]
-// pub struct SetSplMint<'info> {
-// 	#[account(signer)]
-// 	authority: AccountInfo<'info>,
-// 	spl_mint: Account<'info, Mint>,
-// 	raffle_account: Account<'info, RaffleAccount>,
-// }
 
-// #[account]
-// pub struct RaffleAccount {
-//     pub authority: Pubkey, //32
-// 	pub spl_mint: Pubkey, //32 
-// 	pub raffle_winner: Pubkey, //32 
-// 	pub claimed: bool, //1
-// 	pub timestamp: i64, //8
-//     pub reward_mint: Pubkey, //32
-//     pub reward_authority_bump: u8, //8
-// 	// pub valid_minters: Vec<u8>
-// }
-
-#[error]
-pub enum MyError {
-    #[msg("This is an error message clients will automatically display")]
-    Hello,
-    HelloNoMsg = 123,
-    HelloNext,
-    HelloCustom,
+#[account]
+pub struct RaffleAccount {
+    pub authority: Pubkey, //32
+	pub spl_mint: Pubkey, //32 
+	pub raffle_winner: Pubkey, //32 
+	pub claimed: bool, //1
+	pub timestamp: i64, //8
+    pub reward_mint: Pubkey, //32
+    pub reward_authority_bump: u8, //8
+	// pub valid_minters: Vec<u8>
 }
