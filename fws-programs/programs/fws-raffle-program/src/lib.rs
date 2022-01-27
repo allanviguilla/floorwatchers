4dfbc4e10bd3d71b498c142bbb51b39583f3a90f
Khalil Brown <knb6@njit.edu>
2022-01-27 01:05:16 -0500

    raffle-entries

--- fws-programs/programs/fws-raffle-program/src/lib.rs
+++ fws-programs/programs/fws-raffle-program/src/lib.rs
@@ -6,72 +6,101 @@ declare_id!("7HGqiMfjvBZC8632q2YEePfU2m5BhzTmjkY11wsXyS8q");
 
 #[program]
 pub mod fws_raffle_program {
-    use super::*;
-    // pub fn create(ctx: Context<Create>) -> ProgramResult {
-	// 	ctx.accounts.raffle_account.authority = *ctx.accounts.authority.key;
-	// 	ctx.accounts.raffle_account.claimed = false;
-	// 	ctx.accounts.raffle_account.reward_mint = *ctx.accounts.reward_mint.to_account_info().key;
-	// 	// ctx.accounts.raffle_account.reward_authority_bump = ;
-    //     Ok(())
-    // }
-	
-    pub fn draw(ctx: Context<Draw>, holders: Vec<Vec<u8>>) -> ProgramResult {
-		msg!("Hello World!");
+	use super::*;
+	pub fn create(ctx: Context<Create>) -> ProgramResult {
+		ctx.accounts.raffle_account.raffle_authority = *ctx.accounts.raffle_authority.key;
+		ctx.accounts.raffle_account.claimed = false;
+		// ctx.accounts.raffle_account.reward_mint = *ctx.accounts.reward_mint.to_account_info().key;
+		// ctx.accounts.raffle_account.reward_authority_bump = ;
+		msg!("Raffle Account Created: {}", ctx.accounts.raffle_account.raffle_authority);
+		Ok(())
+	}
+
+	pub fn add_raffle_entry(ctx: Context<AddRaffleEntry>, holders: Vec<Vec<u8>>) -> ProgramResult{
+		let raffle_account = &mut ctx.accounts.raffle_account;
+		let account = raffle_account.to_account_info();
+		// let mut data = account.data.borrow();
+
+		msg!("Data, {:?}", account);
+
 		for buffer in holders {
 			let tmp_buffer = buffer.as_slice();
 			let mint = str::from_utf8(tmp_buffer);
+			// if raffle_account.holders.is_some(){
 
+			// }
 			msg!("{:?}", mint);
-			
 		}
-		
-		// msg!("{}", mints[1]);
-		msg!("account: {}", ctx.accounts.authority.key);
-		// let mut mint_list = vec!(mints);
-        Ok(())
-		// Err((MyError))
-    }
-    // pub fn claim(ctx: Context<Claim>) -> ProgramResult {
+		msg!("account: {}", ctx.accounts.raffle_authority.key);
+		Ok(())
+	}
+	// pub fn draw(ctx: Context<Draw>, holders: Vec<Vec<u8>>) -> ProgramResult {
 
-    //     Ok(())
-    // }
+	// 	// for buffer in holders {
+	// 	// 	let tmp_buffer = buffer.as_slice();
+	// 	// 	let mint = str::from_utf8(tmp_buffer);
+	// 	// 	// if raffle_account.holders.is_some(){
 
+	// 	// 	// }
+	// 	// 	msg!("{:?}", mint);
+	// 	// }
+	// 	// msg!("account: {}", ctx.accounts.authority.key);
+
+	// 	Ok(())
+	// 	// Err((MyError))
+	// }
+	// pub fn claim(ctx: Context<Claim>) -> ProgramResult {
+	// 	Ok(())
+	// }
 }
 
 #[derive(Accounts)]
 pub struct Create<'info> {
-	#[account(signer)]
-	authority: AccountInfo<'info>,
+	#[account(mut, signer)]
+	raffle_authority: AccountInfo<'info>,
 	#[account(zero)]
 	raffle_account: Account<'info, RaffleAccount>,
-	reward_mint: Account<'info, Mint>,
-	reward_authority: AccountInfo<'info>
+	// reward_mint: Account<'info, Mint>,
+	// reward_authority: AccountInfo<'info>,
+	pub system_program: AccountInfo<'info>,
+	pub rent: Sysvar<'info, Rent>,
 }
+
+// #[derive(Accounts)]
+// pub struct Draw<'info> {
+// 	#[account(signer)]
+// 	authority: AccountInfo<'info>,
+// }
+
+// #[derive(Accounts)]
+// pub struct Claim<'info> {
+// 	#[account(signer,
+// 	constraint = raffle_account.to_account_info().key == authority.key )]
+// 	authority: AccountInfo<'info>, //winner of the raffle
+// 	raffle_account: Account<'info, RaffleAccount>,
+// }
 
 #[derive(Accounts)]
 #[instruction(holders: Vec<Vec<u8>>)]
-pub struct Draw<'info> {
-	#[account(signer)]
-	authority: AccountInfo<'info>,
+pub struct AddRaffleEntry<'info> {
+    #[account(mut)]
+    raffle_account: Account<'info, RaffleAccount>,
+    raffle_authority: Signer<'info>,
 }
-
-#[derive(Accounts)]
-pub struct Claim<'info> {
-	#[account(signer,
-	constraint = raffle_account.to_account_info().key == authority.key )]
-	authority: AccountInfo<'info>, //winner of the raffle
-	raffle_account: Account<'info, RaffleAccount>,
-}
-
 
 #[account]
 pub struct RaffleAccount {
-    pub authority: Pubkey, //32
-	pub spl_mint: Pubkey, //32 
-	pub raffle_winner: Pubkey, //32 
-	pub claimed: bool, //1
-	pub timestamp: i64, //8
-    pub reward_mint: Pubkey, //32
-    pub reward_authority_bump: u8, //8
-	// pub valid_minters: Vec<u8>
+	pub raffle_authority: Pubkey,     //32
+	// pub spl_mint: Pubkey,      //32
+	// pub raffle_winner: Pubkey, //32
+	pub claimed: bool,         //1
+	pub timestamp: i64,        //8
+	// pub reward_mint: Pubkey,   //32
+	// pub reward_authority_bump: u8, //8
+	pub holders: Option<Vec<RaffleAccountData>>
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct RaffleAccountData {
+	pub holder: Pubkey
 }
--- fws-programs/tests/fws-raffle-program.ts
+++ fws-programs/tests/fws-raffle-program.ts
@@ -5,38 +5,68 @@ import { NodeWallet } from '@project-serum/anchor/dist/cjs/provider';
 import { PublicKey } from '@solana/web3.js';
 
 describe('fws-raffle-program', () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const holders = require('./holders.json');
+
     // Configure the client to use the local cluster.
     anchor.setProvider(anchor.Provider.env());
 
     const program = anchor.workspace.FwsRaffleProgram as Program<FwsRaffleProgram>;
-
+    const raffleAccount = anchor.web3.Keypair.generate();
     const payer = anchor.web3.Keypair.generate();
-	//replace this with array of holders 
-    const holders = ['lalala', 'astasfa'];
+    //replace this with array of holders
+    // const holders = ['lalala', 'astasfa'];
     const buff_holders = holders.map((minter) => {
         return Buffer.from(minter);
     });
-	// new anchor.BN(minters)
-    console.log(buff_holders);
-    it('Draw!', async () => {
-        await anchor.Provider.env()
-            .connection.confirmTransaction(
-                await anchor.Provider.env().connection.requestAirdrop(payer.publicKey, 10000000000),
-                'processed'
-            )
-            .catch((e) => console.log(e));
+    // new anchor.BN(minters)
+    // console.log(buff_holders);
+    it('Create!', async () => {
+        console.log(raffleAccount.publicKey);
         try {
-            const tx = await program.rpc.draw(
-				buff_holders, 
-				{
+            const tx = await program.rpc.create({
                 accounts: {
-                    authority: payer.publicKey,
+                    raffleAuthority: payer.publicKey,
+                    raffleAccount: raffleAccount.publicKey,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                    rent: anchor.web3.SYSVAR_RENT_PUBKEY,
                 },
-                signers: [payer],
+                instructions: [await program.account.raffleAccount.createInstruction(raffleAccount)],
+                signers: [raffleAccount, payer],
             });
-            console.log('Your transaction signature', tx);
+            console.log(`Transaction: ${tx}`);
         } catch (e) {
-            console.log(`Error ${e}`);
+            console.log(e);
         }
+        const _raffleAccount = await program.account.raffleAccount.fetch(raffleAccount.publicKey);
+        console.log(_raffleAccount);
     });
+    it('Add Raffle Entry!', async () => {
+        await program.rpc.addRaffleEntry(buff_holders, {
+            accounts: {
+                raffleAccount: raffleAccount.publicKey,
+                raffleAuthority: raffleAccount.publicKey,
+            },
+			signers: [raffleAccount]
+        });
+    });
+    // it('Draw!', async () => {
+    //     await anchor.Provider.env()
+    //         .connection.confirmTransaction(
+    //             await anchor.Provider.env().connection.requestAirdrop(payer.publicKey, 10000000000),
+    //             'processed'
+    //         )
+    //         .catch((e) => console.log(e));
+    //     try {
+    //         const tx = await program.rpc.draw(buff_holders, {
+    //             accounts: {
+    //                 authority: payer.publicKey,
+    //             },
+    //             signers: [payer],
+    //         });
+    //         console.log('Your transaction signature', tx);
+    //     } catch (e) {
+    //         console.log(`Error ${e}`);
+    //     }
+    // });
 });
